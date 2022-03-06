d1beb020cbd97568376919733c6ff50e001407e1
Wojciech Zmuda <zmuda.w@gmail.com>
2022-03-06 21:57:18 +0100

    arrays-and-slices: use append() so the result slice is always a good size

--- arrays-and-slices/sumall.go
+++ arrays-and-slices/sumall.go
@@ -3,9 +3,17 @@ package main
 // variadic functions!
 func SumAll(numbersToSum ...[]int) []int {
 	// make(t Type size ...IntegerType) Type creates a slice
-	sums := make([]int, len(numbersToSum))
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	// sums := make([]int, len(numbersToSum))
+	// for i, numbers := range numbersToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
+
+	var sums []int
+	for _, numbers := range numbersToSum {
+		// append(slice []Type, elems ...Type) []Type
+		// creates a new slice of Type with contents of slice
+		// plus contents of elems
+		sums = append(sums, Sum(numbers))
 	}
 
 	return sums
